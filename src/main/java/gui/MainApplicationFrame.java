c8bc8ad651f2ba922cdc0481bad5c7392f186d1b
alina <129005794+alinochkaaaaaa@users.noreply.github.com>
2026-03-29 16:02:22 +0500

    combine all start tasks

--- src/main/java/gui/MainApplicationFrame.java
+++ src/main/java/gui/MainApplicationFrame.java
@@ -5,6 +5,12 @@ import model.RobotModel;
 import java.awt.Dimension;
 import java.awt.Toolkit;
 import java.awt.event.KeyEvent;
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.util.Properties;
 
 import javax.swing.JDesktopPane;
 import javax.swing.JFrame;
@@ -12,6 +18,7 @@ import javax.swing.JInternalFrame;
 import javax.swing.JMenu;
 import javax.swing.JMenuBar;
 import javax.swing.JMenuItem;
+import javax.swing.JOptionPane;
 import javax.swing.SwingUtilities;
 import javax.swing.UIManager;
 import javax.swing.UnsupportedLookAndFeelException;
@@ -21,123 +28,254 @@ import log.Logger;
 public class MainApplicationFrame extends JFrame
 {
     private final JDesktopPane desktopPane = new JDesktopPane();
-    // панель, которая может содержать внутренние окна
     private final RobotModel robotModel = new RobotModel();
     private RobotCoordinatesWindow coordinatesWindow;
 
     public MainApplicationFrame() {
-        //Make the big window be indented 50 pixels from each edge
-        //of the screen.
-        int inset = 50;        
+        configureMainFrame();
+        createWindows();
+        setJMenuBar(createMenuBar());
+        setDefaultCloseOperation(DO_NOTHING_ON_CLOSE);
+
+        setupExitHandler();
+        loadWindowPositions();
+    }
+
+    private void configureMainFrame() {
+        int inset = 50;
         Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();
         setBounds(inset, inset,
-            screenSize.width  - inset*2,
-            screenSize.height - inset*2);
-
+                screenSize.width  - inset*2,
+                screenSize.height - inset*2);
         setContentPane(desktopPane);
-        
-
-        // окно лога для отображения сообщений
-        LogWindow logWindow = createLogWindow();
-        addWindow(logWindow);
-
-        // игровое окно с роботом
-        GameWindow gameWindow = new GameWindow(robotModel);
-        gameWindow.setSize(400,  400);
-        addWindow(gameWindow);
-
-        coordinatesWindow = new RobotCoordinatesWindow(robotModel);
-        addWindow(coordinatesWindow);
-
-        setJMenuBar(generateMenuBar());
-        setDefaultCloseOperation(EXIT_ON_CLOSE);
     }
-    
-    protected LogWindow createLogWindow()
-    {
+
+    private void createWindows() {
+        addWindow(createLogWindow());
+        addWindow(createGameWindow());
+        addWindow(createCoordinatesWindow());
+    }
+
+    private LogWindow createLogWindow() {
         LogWindow logWindow = new LogWindow(Logger.getDefaultLogSource());
-        logWindow.setLocation(10,10);
+        logWindow.setLocation(10, 10);
         logWindow.setSize(300, 800);
-        setMinimumSize(logWindow.getSize());
+        logWindow.setTitle("Протокол работы");
+        logWindow.setMinimumSize(logWindow.getSize());
         logWindow.pack();
         Logger.debug("Протокол работает");
         return logWindow;
     }
-    
-    protected void addWindow(JInternalFrame frame)
-    {
+
+    private GameWindow createGameWindow() {
+        GameWindow gameWindow = new GameWindow(robotModel);
+        gameWindow.setSize(400, 400);
+        gameWindow.setTitle("Игровое поле");
+        return gameWindow;
+    }
+
+    private RobotCoordinatesWindow createCoordinatesWindow() {
+        coordinatesWindow = new RobotCoordinatesWindow(robotModel);
+        coordinatesWindow.setTitle("Координаты робота");
+        return coordinatesWindow;
+    }
+
+    private void addWindow(JInternalFrame frame) {
         desktopPane.add(frame);
         frame.setVisible(true);
     }
-    
-    private JMenuBar generateMenuBar()
-    {
+
+    private void setupExitHandler() {
+        addWindowListener(new WindowAdapter() {
+            @Override
+            public void windowClosing(WindowEvent e) {
+                exitApplication();
+            }
+        });
+    }
+
+    private JMenuBar createMenuBar() {
         JMenuBar menuBar = new JMenuBar();
+        menuBar.add(createFileMenu());
+        menuBar.add(createLookAndFeelMenu());
+        menuBar.add(createTestMenu());
+        return menuBar;
+    }
 
-        JMenu windowsMenu = new JMenu("Окна");
-        windowsMenu.setMnemonic(KeyEvent.VK_W);
+    private JMenu createFileMenu() {
+        JMenu fileMenu = new JMenu("Файл");
+        fileMenu.setMnemonic(KeyEvent.VK_F);
 
-        JMenuItem showCoordinatesWindow = new JMenuItem("Показать координаты");
-        showCoordinatesWindow.addActionListener(e -> {
+        JMenuItem showCoordinatesItem = new JMenuItem("Показать координаты", KeyEvent.VK_C);
+        showCoordinatesItem.addActionListener(e -> {
             if (coordinatesWindow != null && !coordinatesWindow.isVisible()) {
                 coordinatesWindow.setVisible(true);
             }
         });
-        windowsMenu.add(showCoordinatesWindow);
+        fileMenu.add(showCoordinatesItem);
 
+        fileMenu.addSeparator();
+
+        JMenuItem exitMenuItem = new JMenuItem("Выход", KeyEvent.VK_Q);
+        exitMenuItem.addActionListener(e -> exitApplication());
+        fileMenu.add(exitMenuItem);
+
+        return fileMenu;
+    }
+
+    private JMenu createLookAndFeelMenu() {
         JMenu lookAndFeelMenu = new JMenu("Режим отображения");
         lookAndFeelMenu.setMnemonic(KeyEvent.VK_V);
         lookAndFeelMenu.getAccessibleContext().setAccessibleDescription(
                 "Управление режимом отображения приложения");
-        
-        {
-            JMenuItem systemLookAndFeel = new JMenuItem("Системная схема", KeyEvent.VK_S);
-            systemLookAndFeel.addActionListener((event) -> {
-                setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
-                this.invalidate();
-            });
-            lookAndFeelMenu.add(systemLookAndFeel);
-        }
 
-        {
-            JMenuItem crossplatformLookAndFeel = new JMenuItem("Универсальная схема", KeyEvent.VK_S);
-            crossplatformLookAndFeel.addActionListener((event) -> {
-                setLookAndFeel(UIManager.getCrossPlatformLookAndFeelClassName());
-                this.invalidate();
-            });
-            lookAndFeelMenu.add(crossplatformLookAndFeel);
-        }
+        JMenuItem systemLookAndFeel = new JMenuItem("Системная схема", KeyEvent.VK_S);
+        systemLookAndFeel.addActionListener(e -> {
+            setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
+            invalidate();
+        });
+        lookAndFeelMenu.add(systemLookAndFeel);
 
+        JMenuItem crossplatformLookAndFeel = new JMenuItem("Универсальная схема", KeyEvent.VK_S);
+        crossplatformLookAndFeel.addActionListener(e -> {
+            setLookAndFeel(UIManager.getCrossPlatformLookAndFeelClassName());
+            invalidate();
+        });
+        lookAndFeelMenu.add(crossplatformLookAndFeel);
+
+        return lookAndFeelMenu;
+    }
+
+    private JMenu createTestMenu() {
         JMenu testMenu = new JMenu("Тесты");
         testMenu.setMnemonic(KeyEvent.VK_T);
         testMenu.getAccessibleContext().setAccessibleDescription(
                 "Тестовые команды");
-        
-        {
-            JMenuItem addLogMessageItem = new JMenuItem("Сообщение в лог", KeyEvent.VK_S);
-            addLogMessageItem.addActionListener((event) -> {
-                Logger.debug("Новая строка");
-            });
-            testMenu.add(addLogMessageItem);
-        }
 
-        menuBar.add(windowsMenu);
-        menuBar.add(lookAndFeelMenu);
-        menuBar.add(testMenu);
-        return menuBar;
+        JMenuItem addLogMessageItem = new JMenuItem("Сообщение в лог", KeyEvent.VK_S);
+        addLogMessageItem.addActionListener(e -> Logger.debug("Новая строка"));
+        testMenu.add(addLogMessageItem);
+
+        return testMenu;
     }
-    
-    private void setLookAndFeel(String className)
-    {
-        try
-        {
+
+    private void exitApplication() {
+        int result = JOptionPane.showOptionDialog(
+                this,
+                "Вы действительно хотите выйти?",
+                "Подтверждение выхода",
+                JOptionPane.YES_NO_OPTION,
+                JOptionPane.QUESTION_MESSAGE,
+                null,
+                new String[]{"Да", "Нет"},
+                "Нет"
+        );
+
+        if (result == 0) {
+            saveWindowPositions();
+            System.exit(0);
+        }
+    }
+
+    private void setLookAndFeel(String className) {
+        try {
             UIManager.setLookAndFeel(className);
             SwingUtilities.updateComponentTreeUI(this);
+        } catch (ClassNotFoundException | InstantiationException
+                 | IllegalAccessException | UnsupportedLookAndFeelException e) {
+            // ignore
         }
-        catch (ClassNotFoundException | InstantiationException
-            | IllegalAccessException | UnsupportedLookAndFeelException e)
-        {
-            // just ignore
+    }
+
+    private void saveWindowPositions() {
+        try {
+            String userHome = System.getProperty("user.home");
+            File configFile = new File(userHome, ".robots_window_config.xml");
+            Properties props = new Properties();
+
+            JInternalFrame[] frames = desktopPane.getAllFrames();
+            StringBuilder zOrder = new StringBuilder();
+
+            for (JInternalFrame frame : frames) {
+                String frameName = frame.getTitle();
+
+                if (zOrder.length() > 0) {
+                    zOrder.append(",");
+                }
+                zOrder.append(frameName);
+
+                props.setProperty(frameName + ".x", String.valueOf(frame.getX()));
+                props.setProperty(frameName + ".y", String.valueOf(frame.getY()));
+                props.setProperty(frameName + ".width", String.valueOf(frame.getWidth()));
+                props.setProperty(frameName + ".height", String.valueOf(frame.getHeight()));
+                props.setProperty(frameName + ".icon", String.valueOf(frame.isIcon()));
+                props.setProperty(frameName + ".maximum", String.valueOf(frame.isMaximum()));
+            }
+
+            props.setProperty("window.zorder", zOrder.toString());
+            props.storeToXML(new FileOutputStream(configFile), "Robot Application Window Positions");
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    private void loadWindowPositions() {
+        try {
+            String userHome = System.getProperty("user.home");
+            File configFile = new File(userHome, ".robots_window_config.xml");
+
+            if (!configFile.exists()) {
+                return;
+            }
+
+            Properties props = new Properties();
+            props.loadFromXML(new FileInputStream(configFile));
+
+            JInternalFrame[] frames = desktopPane.getAllFrames();
+
+            for (JInternalFrame frame : frames) {
+                String frameName = frame.getTitle();
+
+                String xStr = props.getProperty(frameName + ".x");
+                String yStr = props.getProperty(frameName + ".y");
+                if (xStr != null && yStr != null) {
+                    frame.setLocation(Integer.parseInt(xStr), Integer.parseInt(yStr));
+                }
+
+                String wStr = props.getProperty(frameName + ".width");
+                String hStr = props.getProperty(frameName + ".height");
+                if (wStr != null && hStr != null) {
+                    frame.setSize(Integer.parseInt(wStr), Integer.parseInt(hStr));
+                }
+
+                String maxStr = props.getProperty(frameName + ".maximum");
+                if ("true".equals(maxStr)) {
+                    try { frame.setMaximum(true); } catch (Exception e) {}
+                }
+
+                String iconStr = props.getProperty(frameName + ".icon");
+                if ("true".equals(iconStr) && !"true".equals(maxStr)) {
+                    try { frame.setIcon(true); } catch (Exception e) {}
+                }
+            }
+
+            String zOrderStr = props.getProperty("window.zorder");
+            if (zOrderStr != null && !zOrderStr.isEmpty()) {
+                String[] frameTitles = zOrderStr.split(",");
+                for (int i = frameTitles.length - 1; i >= 0; i--) {
+                    for (JInternalFrame frame : frames) {
+                        if (frame.getTitle().equals(frameTitles[i])) {
+                            desktopPane.setComponentZOrder(frame, 0);
+                            frame.toFront();
+                            break;
+                        }
+                    }
+                }
+            }
+
+        } catch (Exception e) {
+            e.printStackTrace();
         }
     }
 }
